eb274a6521eb085c0f1e485d714fffeb785307bd
Louis-Marie RICHARD <LMR@MacBook-Pro-de-Louis-Marie.local>
2014-10-16 15:15:29 +0200

    Initial commit

---
+++ resources/js/valid_form.js
@@ -0,0 +1,63 @@
+
+function checkForm()
+  {
+      erreur = "";
+      console.log($('input[name^=passwordinput_saisie]').val());
+
+    if($('input[name^=passwordinput_saisie]').val() != "" && $('input[name^=passwordinput_saisie]').val() == $('input[name^=passwordinput_confirm]').val()) {
+
+        if($('input[name^=passwordinput_saisie]').val().length() < 6) {
+        erreur +="Erreur: Le mot de passe doit contenir au moins 6 caractères!";
+            $('input[name^=passwordinput_saisie]').focus();
+        return erreur;
+        }
+    if($('input[name^=passwordinput_saisie]').val() == $('input[name^=textinput_email]').val()) {
+        erreur +="Erreur: Le mot de passe doit être différent du nom d'utilisateur!";
+        $('input[name^=passwordinput_saisie]').focus();
+        return erreur;
+        }
+    re = /[0-9]/;
+    if(!re.test($('input[name^=passwordinput_saisie]').val())) {
+        erreur +="Erreur: Le mot de passe doit contenir au moins un chiffre (0-9)!";
+        $('input[name^=passwordinput_saisie]').focus();
+        return erreur;
+        }
+    re = /[a-z]/;
+    if(!re.test($('input[name^=passwordinput_saisie]').val())) {
+        erreur +="Erreur: Le mot de passe doit contenir au moins une lettre minuscule (a-z)!";
+        $('input[name^=passwordinput_saisie]').focus();
+        return erreur;
+        }
+    re = /[A-Z]/;
+    if(!re.test($('input[name^=passwordinput_saisie]').val())) {
+        erreur +="Erreur: Le mot de passe doit contenir au moins une lettre majuscule (A-Z)!";
+        $('input[name^=passwordinput_saisie]').focus();
+        return erreur;
+        }
+    } else {
+        erreur +="Erreur: Vérifier que le mot de passe a été saisi!";
+        $('input[name^=passwordinput_saisie]').focus();
+        return erreur;
+        }
+
+    //alert("Vous avez entré un mot de passe valide: " + $('input[name^=passwordinput_saisie]').val());
+    return true;
+}
+
+
+$("#button_valider").click(
+        function()
+        {
+            console.log("button_valider CLICK");
+            result = checkForm();
+
+            if (result != true)
+            {
+                content = ""
+
+                $('#erreur_catch').val(result);
+                $('#erreur_catch').show();
+            }
+            return false;
+        }
+);
